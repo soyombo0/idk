de94fab940dc25ac7931e658206bd3b00e8803e0
soyombo <soyombo0@naver.com>
2022-12-31 06:14:23 +0300

    Initial Commit

---
+++ idk/idkApp.swift
@@ -0,0 +1,17 @@
+//
+//  idkApp.swift
+//  idk
+//
+//  Created by Soyombo Mantaagiin on 31.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct idkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
